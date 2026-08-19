06d45a637954bfec263c5b4b087d4d6d14bfae48
antosorrentino2010-rgb <antosorrentino2010@gmail.com>
2026-08-19 02:17:00 +0200

    Update plugin versions in build.gradle.kts
    Updated Android and Kotlin plugin versions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,4 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-}
-
-android {
-    namespace = "com.spiritelli.app"
-    compileSdk = 35
-
-    defaultConfig {
-        applicationId = "com.spiritelli.app"
-        minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-}
-
-dependencies {
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }
